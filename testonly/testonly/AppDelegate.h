fc63cb4166a78830d334508c1abcd19d96a75205
tongyuhu <1449807759@qq.com>
2015-12-11 14:38:07 +0800

    test

---
+++ testonly/testonly/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testonly
+//
+//  Created by tongyuhu on 15/11/24.
+//  Copyright © 2015年 SHmoyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testonly/testonly/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  testonly
+//
+//  Created by tongyuhu on 15/11/24.
+//  Copyright © 2015年 SHmoyi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
